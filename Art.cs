52406cc3761f8ff0372c5093aa039df901782b85
epicsaxgandalf <43189142+epicsaxgandalf@users.noreply.github.com>
2022-05-04 17:49:40 -0700

    added spite location and entity manager

---
+++ Art.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGame
+{
+    static class Art
+    {
+        public static Texture2D Player { get; private set; }
+        public static Texture2D Seeker { get; private set; }
+        public static Texture2D Wanderer { get; private set; }
+        public static Texture2D Bullet { get; private set; }
+        public static Texture2D Pointer { get; private set; }
+
+        public static Texture2D wallSprite { get; private set; }
+        public static Texture2D wallCornerLeftSprite { get; private set; }
+        public static Texture2D wallCornerRightSprite { get; private set; }
+        public static Texture2D chestSprite { get; private set; }
+
+
+        public static void Load(ContentManager content)
+        {
+            Player = content.Load<Texture2D>("Player");
+            Seeker = content.Load<Texture2D>("Seeker");
+            Wanderer = content.Load<Texture2D>("Wanderer");
+            Bullet = content.Load<Texture2D>("Bullet");
+            Pointer = content.Load<Texture2D>("Pointer");
+
+            wallSprite = content.Load<Texture2D>("wall_mid");
+            wallCornerLeftSprite = content.Load<Texture2D>("wall_corner_left");
+            wallCornerRightSprite = content.Load<Texture2D>("wall_corner_right");
+            chestSprite = content.Load<Texture2D>("chest_empty_open_anim_f0");
+        }
+    }
+}
